1730c548043e8fcbc32def5365a81267b46df5bb
Mitjanit <42413333+mitjanit@users.noreply.github.com>
2022-02-02 09:56:03 +0100

    Table React JSON Fetch

--- src/App.js
+++ src/App.js
@@ -1,25 +1,52 @@
-import logo from './logo.svg';
+import React, { Component } from 'react';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component {
+  
+  constructor(){
+      super(); 
+      this.state = {
+        data: [ ]
+      }
+  }
+
+  componentDidMount() {
+    fetch('http://jsonplaceholder.typicode.com/posts')
+      .then(response => response.json())
+      .then(data => this.setState({ data }));
+  }
+  
+  render() {
+              
+      return (
+        <div className="App">
+        <div className="container">
+            <h1>Table JSON React </h1>
+            <table className="table table-striped table-bordered">
+              <thead>
+                <tr>
+                  <th>Id</th>
+                  <th>UserId</th>
+                  <th>Title</th>
+                  <th>Body</th>
+                </tr>
+              </thead>
+              <tbody>{this.state.data.map(function(item, key) {
+                     return (
+                        <tr key = {key}>
+                            <td>{item.id}</td>
+                            <td>{item.userId}</td>
+                            <td>{item.title}</td>
+                            <td>{item.body}</td>
+                        </tr>
+                      )
+                   
+                   })}</tbody>
+             </table>
+           </div>
+         </div>
+      )
+    }
 }
 
 export default App;
